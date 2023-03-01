0112b134b37b2dd349dd4b7cf26a566fddbfe073
ornellapiccinni <59055415+ornellapiccinni@users.noreply.github.com>
2023-03-01 14:51:58 +0000

    Co-authored-by: mseglar <mseglar@users.noreply.github.com>

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import pandas as pd
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 
-lelel
 
-dsfs
+# Read file and extract them by columns. 
+# Add an index so that it is easy to be parsed when we do a time-fluction study
+
+# Do 2D plot of laser power vs. voltage (histograms of each variable)
+
+# Compute residuals 
+
