dd49101b70cb3836440163620b75194834c27404
ornellapiccinni <59055415+ornellapiccinni@users.noreply.github.com>
2023-03-01 15:20:10 +0000

    test

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ nameDir = filename.split('/')[-1].split('.')[0]
 run = pd.read_csv(filename, delimiter ='\t')
 print(run)
 # Add an index so that it is easy to be parsed when we do a time-fluction study
-run.plot(kind='scatter', x='KeithleyMeanV', y='PowerMeterMeanW', s=3, c='green')
 
 isExist = os.path.exists(nameDir)
 if not isExist:
@@ -22,6 +21,7 @@ if not isExist:
    os.makedirs(nameDir)
    print("The new directory is created!")
 
+run.plot(kind='scatter', x='KeithleyMeanV', y='PowerMeterMeanW', s=3, c='green')
 plt.savefig(nameDir+'/1.png')
 
 # Do 2D plot of laser power vs. voltage (histograms of each variable)
