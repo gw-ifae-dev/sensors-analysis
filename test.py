d5fe1732a4358a5b458d506cec4d955af0018a50
ornellapiccinni <59055415+ornellapiccinni@users.noreply.github.com>
2023-03-01 15:15:05 +0000

    test

--- test.py
+++ test.py
@@ -10,19 +10,19 @@ import os
 
 # Read file and extract them by columns. 
 filename = '/workspaces/codespaces-jupyter/data/PM_vs_PD_Sensitivity_Final_1.txt'
-name = filename.split('/')[-1].split('.')[0]
+nameDir = filename.split('/')[-1].split('.')[0]
 run = pd.read_csv(filename, delimiter ='\t')
 print(run)
 # Add an index so that it is easy to be parsed when we do a time-fluction study
-run.plot(kind='scatter', x='KeithleyMeanV', y='PowerMeterMeanW', s=35, c='green')
+run.plot(kind='scatter', x='KeithleyMeanV', y='PowerMeterMeanW', s=3, c='green')
 
-isExist = os.path.exists(path)
+isExist = os.path.exists(nameDir)
 if not isExist:
    # Create a new directory because it does not exist
-   os.makedirs(path)
+   os.makedirs(nameDir)
    print("The new directory is created!")
 
-plt.savefig(name+'/1.png')
+plt.savefig(nameDir+'/1.png')
 
 # Do 2D plot of laser power vs. voltage (histograms of each variable)
 # NicePlots(run)
